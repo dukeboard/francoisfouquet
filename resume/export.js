7c92dc8dc5fd5e50a489631a9f1941444f35c36b
Francois Fouquet <fouquet.f@gmail.com>
2015-04-20 17:12:31 +0200

    push resume

---
+++ resume/export.js
@@ -0,0 +1,17 @@
+var mustache = require('mustache');
+var fs = require('fs');
+var path = require('path');
+
+var layout = fs.readFileSync('layout.html', "utf8");
+var resumeObject = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'resume.json'), 'utf8'));
+for(var subResumeElem in resumeObject){
+    if(resumeObject[subResumeElem].length !== undefined){
+        resumeObject[subResumeElem][resumeObject[subResumeElem].length-1].last = true;
+    }
+}
+var payload = mustache.to_html(layout, resumeObject);
+fs.writeFile('resume.html', payload, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+}); 
